66f3c526d8411fadb89de219c896683fb9ff2bed
nathaliemar <n.marten.95@gmail.com>
2025-06-24 17:00:02 +0200

    Implement WorkoutListPage with workout fetching functionality

--- src/views/WorkoutListPage.vue
+++ src/views/WorkoutListPage.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="flex justify-center">
-    <h1 class="text-xl font-semibold">Manage workouts</h1>
+  <div class="flex flex-col justify-center">
+    <div>
+      <h1 class="text-xl font-semibold">Manage workouts</h1>
+    </div>
+    <div>
+      <p v-text="workouts"></p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import { api } from "../services/api";
+
 export default {
   name: "WorkoutListPage",
+  data() {
+    return {
+      workouts: [],
+    };
+  },
+  mounted() {
+    this.fetchAllWorkouts();
+  },
+  methods: {
+    async fetchAllWorkouts() {
+      try {
+        const response = await api.get("/workouts");
+        this.workouts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
